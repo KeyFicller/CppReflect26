69457612ecf2171686caab1bebeefe1e8e0c9289
zhangyuhui <whuzyh1998@163.com>
2026-05-11 13:34:07 +0800

    hello reflection

--- src/hello_reflection.cpp
+++ src/hello_reflection.cpp
@@ -1,5 +1,5 @@
 #include "test_entry.h"
-#include <experimental/meta>
+#include <meta>
 #include <print>
 #include <string>
 
@@ -7,6 +7,16 @@ struct PodStruct {
     int m_mem_int;
     double m_mem_real;
     std::string m_mem_str;
+};
+
+enum class WeekDay{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
 };
 
 template <typename T>
@@ -19,8 +29,22 @@ void print_each_field(const T& _object)
     }
 }
 
+template <typename E>
+void print_each_enumerator(const E& _object)
+{
+    constexpr auto enumerators = std::define_static_array(
+        std::meta::enumerators_of(^^E));
+    template for (constexpr std::meta::info e : enumerators) {
+        constexpr E value = std::meta::extract<E>(std::meta::constant_of(e));
+        std::println("  {}:{}", std::meta::identifier_of(e), _object == value);
+    }
+}
+
 void test_entry::hello_reflection()
 {
     PodStruct object{.m_mem_int = 1, .m_mem_real = 2.0, .m_mem_str = "Hello, Reflection!"};
     print_each_field(object);
+
+    WeekDay day = WeekDay::Tuesday;
+    print_each_enumerator(day);
 }
